5aa9b1475c83ce7d774cb191890ec76da42503cc
EveSantana <87048983+EveSantana@users.noreply.github.com>
2022-10-29 18:39:45 -0300

    Add files via upload

--- Lab2/Exercicio2.py
+++ Lab2/Exercicio2.py
@@ -1,22 +1,41 @@
-import os , time
+import os 
+import time
+from pwd import getpwuid
 tamanhos={}
 diretorio=os.getcwd()
+
 print(diretorio)
 print("Diretorio: ",diretorio)
-print(os.listdir(diretorio))
+arquivos=(os.listdir(diretorio))
+for indices in arquivos:
+      print("Arquivo: ",indices)
+print()    
+for indices in arquivos:
+      sub_caminho=os.path.join(diretorio, indices)
+      tamanho=os.path.getsize(sub_caminho)
+      ultima_att=time.ctime(os.path.getmtime(sub_caminho))
+      dt_criacao=time.ctime(os.path.getctime(sub_caminho))
+      author_id=os.stat(sub_caminho).st_uid
+      author=getpwuid(author_id).pw_name
+      print("Arquivo: ",indices,"Tamanho: ",tamanho,"bytes"," Atualizado por último: ",ultima_att," Criado: ",dt_criacao," Autor:",author)
+      print("\n")
 
-print("\n")
-for raiz, pastas, arquivos in os.walk(diretorio, topdown=False):
+for raiz, pastas, arquivos in os.walk(diretorio, topdown=True):
    for name in pastas:
       caminho=os.path.join(raiz, name)
       print("Diretorio: ",caminho)
-      #for nome_arquivo in (caminho):
       files=(os.listdir(caminho))
-      print(files)
+      
+      for j in files:
+         print("Arquivo: ",j)
+      print()
+      
       for i in files:
          sub_caminho=os.path.join(caminho, i)
          tamanho=os.path.getsize(sub_caminho)
          ultima_att=time.ctime(os.path.getmtime(sub_caminho))
          dt_criacao=time.ctime(os.path.getctime(sub_caminho))
-         author=os.stat(sub_caminho).st_uid
-         print(tamanho,"bytes; \n Atualizado por último ",ultima_att,"; \n Criado ",dt_criacao,"\n\n","Autor:",author)
+         author_id=os.stat(sub_caminho).st_uid
+         author=getpwuid(author_id).pw_name
+         print("Arquivo: ",i,"Tamanho: ",tamanho,"bytes"," Atualizado por último: ",ultima_att," Criado: ",dt_criacao," Autor:",author)
+         print()

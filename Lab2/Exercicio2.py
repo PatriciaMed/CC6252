fbb37f5528cd06fbbab4312e029a4350824e50b1
PatriciaMed <99925809+PatriciaMed@users.noreply.github.com>
2022-10-29 16:21:30 -0300

    Update Exercicio2.py

--- Lab2/Exercicio2.py
+++ Lab2/Exercicio2.py
@@ -1,4 +1,4 @@
-import os 
+import os , time
 tamanhos={}
 diretorio=os.getcwd()
 print(diretorio)
@@ -14,18 +14,9 @@ for raiz, pastas, arquivos in os.walk(diretorio, topdown=False):
       files=(os.listdir(caminho))
       print(files)
       for i in files:
-         #print(os.path.getsize(caminho))
          sub_caminho=os.path.join(caminho, i)
-         tamanho=os.path.getsize(ok)
-         ultima_att=os.path.getmtime(sub_caminho)
-         #print(os.path.getmtime(filename))
-         dt_criacao=os.path.getctime(sub_caminho)
-         #print(os.path.getctime(filename))
-         print(tamanho)
-  
-   
-      #tamanhos[name]=(os.stat(os.path.join(raiz, name)).st_size)
-   
-     
-#final=sorted(tamanhos.items(),reverse=True, key=lambda x:x[1])
-#print(final)
+         tamanho=os.path.getsize(sub_caminho)
+         ultima_att=time.ctime(os.path.getmtime(sub_caminho))
+         dt_criacao=time.ctime(os.path.getctime(sub_caminho))
+         author=os.stat(sub_caminho).st_uid
+         print(tamanho,"bytes; \n Atualizado por último ",ultima_att,"; \n Criado ",dt_criacao,"\n\n","Autor:",author)
